4f2b96d08a8193383fdd3f508a2d6f51825d4188
remya0804 <remyaraju2205@gmail.com>
2024-08-30 18:58:16 +0800

    deployed

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ base: "/anria-updated", plugins: [react()], }) 
+export default defineConfig({ base: "/anria", plugins: [react()], }) 
